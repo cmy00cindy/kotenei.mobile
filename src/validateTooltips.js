11f2274ef8ee0e0576415603b64798dd967f2a4f
vfasky <vfasky@gmail.com>
2014-09-24 13:34:19 +0800

    init

---
+++ src/validateTooltips.js
@@ -0,0 +1,71 @@
+﻿/*
+ * validate扩展  使用tooltips显示错误
+ * @date:2014-09-06
+ * @author:kotenei(kotenei@qq.com)
+ */
+define('kotenei/validateTooltips', ['jquery', 'kotenei/validate', 'kotenei/tooltips'], function ($, Validate, Tooltips) {
+
+	/**
+	 * 获取元素错误提示定位
+	 * @param  {object} element - dom
+	 * @return {String}       
+	 */
+	Validate.prototype.getTipsPlacement = function (element) {
+		var name = element.name, placement = "right";
+		if (!this.tipsPlacement) {
+			this.tipsPlacement = this.options.tipsPlacement || {};
+		}
+		if (!this.tipsPlacement[name]) {
+			this.tipsPlacement[name] = placement;
+		} else {
+			placement = this.tipsPlacement[name];
+		}
+		return placement;
+	};
+
+	/**
+	 * 显示tips错误
+	 * @param  {JQuery} $element - dom
+	 * @param  {String} message - 错误信息
+	 * @return {Void}        
+	 */
+	Validate.prototype.showError = function ($element, message) {
+		if (this.checkable($element[0])) {
+			$element = this.validFields.data[$element[0].name];
+		}
+		var placement = this.getTipsPlacement($element[0]);
+		var tooltips = Tooltips.Get($element);
+		if (!tooltips) {
+			tooltips = new Tooltips($element, {
+				content: message,
+				tipClass: 'danger',
+				trigger: 'manual',
+                placement:placement
+			});
+			Tooltips.Set($element, tooltips);
+		} else {
+			tooltips.setContent(message);
+		}
+		tooltips.show();
+		$element.addClass(this.options.errorClass);
+	};
+
+	/**
+	 * 隐藏tips错误
+	 * @param  {JQuery} $element -dom
+	 * @return {Void}  
+	 */
+	Validate.prototype.hideError = function ($element) {
+		if (this.checkable($element[0])) {
+			$element = this.validFields.data[$element[0].name];
+		}
+		var tooltips = Tooltips.Get($element);
+		if (tooltips) {
+			tooltips.hide();
+		}
+		$element.removeClass(this.options.errorClass);
+		
+	};
+
+	return Validate;
+});
